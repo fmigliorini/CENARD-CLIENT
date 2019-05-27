e8f945a1b907731eb63d120cc911d8eb0a50b2f2
Facundo Migliorini <facundo.migliorini@opskins.com>
2019-05-27 13:37:51 -0300

    Bootstrap: Add bootstrap to the proyect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/";
-import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas/";
 
 const sagaMiddleware = createSagaMiddleware();
