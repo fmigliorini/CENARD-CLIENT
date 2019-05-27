4958980504902effd0048944023935adcff84904
Facundo Migliorini <facundo.migliorini@opskins.com>
2019-05-27 15:05:07 -0300

    Header: Fix style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,21 +3,41 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login/">About</Link>
-        </li>
-        <li>
-          <Link to="/register/">Users</Link>
-        </li>
-        <li>
-          <Link to="/events/">Events</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        {`PRODI`}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login/">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register/">Users</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/events/">Events</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/record/">Record</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
