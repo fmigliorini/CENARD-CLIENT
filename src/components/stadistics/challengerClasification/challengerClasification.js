07dc2758f26d67d1bc4a2ac62f0f845a7980f31c
Facundo Migliorini <facundo.migliorini@opskins.com>
2019-05-31 23:33:37 -0300

    Personal stadistics: base component

--- src/components/stadistics/challengerClasification/challengerClasification.js
+++ src/components/stadistics/challengerClasification/challengerClasification.js
@@ -1,10 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { throwStatement } from "@babel/types";
 
 class ChallengerClasification extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return <p>{`challenger clasification`}</p>;
-    }
+    this.state = {
+      stadistics: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      stadistics: [
+        {
+          id: 0,
+          torneo: "CENARD",
+          deporte: "100 metros",
+          promedio: "20 segundos"
+        },
+        {
+          id: 1,
+          torneo: "CENARD",
+          deporte: "200 metros",
+          promedio: "35 segundos"
+        }
+      ]
+    });
+  }
+
+  render() {
+    const { stadistics } = this.state;
+    return (
+      <>
+        <h2 className="text-center">Torneos</h2>
+        <table className="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Torneo</th>
+              <th scope="col">Deporte</th>
+              <th scope="col">Promedio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {stadistics.map(stadistic => {
+              return (
+                <tr>
+                  <th scope="row">{stadistic.id}</th>
+                  <td>{stadistic.torneo}</td>
+                  <td>{stadistic.deporte}</td>
+                  <td>{stadistic.promedio}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
 export default ChallengerClasification;
