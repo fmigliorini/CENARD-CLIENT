25ec32a0b5e9342c7bad89d1b0503ab6038978aa
Facundo Migliorini <facundo.migliorini@opskins.com>
2019-06-18 23:41:35 -0300

    Login complete

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,55 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { loginAction } from "../../actions/login";
 import { useFormState } from "react-use-form-state";
-import './login.css';
+import "./login.css";
 
-const Login = props => {
-  const [formState, { label, email, password }] = useFormState();
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: ""
+    };
+    this.handleSubmit.bind(this);
+    this.handleUsername.bind(this);
+    this.handlePassword.bind(this);
+  }
 
-  const handleSubmit = e => {
+  handleSubmit(e) {
     e.preventDefault();
-    console.log("Login in");
-    props.singIn();
-  };
+    console.log(this.state);
+  }
 
-  return (
-    <>
-      <div class="d-flex justify-content-center h-100">
-        <div class="card">
-          <div class="card-header">
-            <h3>Ingresar</h3>
-          </div>
-          <div class="card-body">
-            <form>
-              <div class="input-group form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Usuario"
-                />
-              </div>
-              <div class="input-group form-group">
-                <input
-                  type="password"
-                  class="form-control"
-                  placeholder="Clave"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="submit"
-                  value="Ingresar"
-                  class="btn float-right login_btn"
-                />
-              </div>
-            </form>
+  handleUsername(event) {
+    this.setState({
+      userName: event.target.value
+    });
+  }
+
+  handlePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div class="d-flex justify-content-center h-100">
+          <div class="card">
+            <div class="card-header">
+              <h3> {`Ingresar`} </h3>
+            </div>
+            <div class="card-body">
+              <form onSubmit={this.handleSubmit}>
+                <div class="input-group form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Usuario"
+                    value={this.state.username}
+                    onChange={this.handleUsername}
+                  />
+                </div>
+                <div class="input-group form-group">
+                  <input
+                    type="password"
+                    class="form-control"
+                    placeholder="Clave"
+                    value={this.state.password}
+                    onChange={this.handlePassword}
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="submit"
+                    value="Ingresar"
+                    class="btn float-right login_btn"
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 const mapDispatchToPros = dispatch => ({
   singIn: () => {
