eeb9d6fb68730a46cf963c4730ed21a30b499222
Facundo Migliorini <facundo.migliorini@opskins.com>
2019-06-19 19:27:58 -0300

    Events sagas complete

--- src/actions/event.js
+++ src/actions/event.js
@@ -1,9 +1,20 @@
-import {
-    GET_EVENT_WATCHER
-} from '../constants/actionTypes';
+import axios from "axios";
+
+import { GET_EVENT_WATCHER } from "../constants/actionTypes";
 
 export const getEvents = () => {
-    return {
-        type: GET_EVENT_WATCHER
-    }
-}
+  return {
+    type: GET_EVENT_WATCHER
+  };
+};
+
+export const getActiveEvents = () => {
+  return axios
+    .get("/events")
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log("error", error);
+    });
+};
--- src/sagas/event.js
+++ src/sagas/event.js
@@ -1,13 +1,12 @@
-import { all, put, takeLatest } from "redux-saga/effects";
+import { all, put, call, takeLatest } from "redux-saga/effects";
 
 import { GET_EVENT, GET_EVENT_WATCHER } from "../constants/actionTypes";
+import { getAllEvents } from '../actions/event';
 
 export function* getEvents() {
-  console.log("SAGA: events");
-  // const res = yield axios.get()....
-  // yield put({type: GET_EVENT, payload: data.res}); // dispatch example from saga
-  console.log("GET EVENTS FROM SAGAS");
-  yield put({ type: GET_EVENT, payload: [{}] });
+  const res = yield call(getAllEvents());
+  console.log("getAllEvents", res );
+  yield put({ type: GET_EVENT, payload: res.data });
 }
 
 export default function* eventSaga() {
